44ef4e3ca3e15ef0f31024b1cc7f4d4dba2e5751
Heiko Seeberger <heiko.seeberger@gmail.com>
2014-05-07 21:57:36 +0200

    Set up project

--- build.sbt
+++ build.sbt
@@ -1,5 +1,7 @@
 Common.settings
- 
+
+resolvers += "spray repo" at "http://repo.spray.io"
+
 libraryDependencies ++= Dependencies.akkamazing
- 
+
 initialCommands := """|import name.heikoseeberger.akkamazing._""".stripMargin
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,14 +5,22 @@ object Version {
   val logback   = "1.1.2"
   val scala     = "2.11.0"
   val scalaTest = "2.1.4"
+  val spray     = "1.3.1-20140423"
+  val sprayJson = "1.2.6"
 }
 
 object Library {
-  val akkaActor      = "com.typesafe.akka" %% "akka-actor"      % Version.akka
-  val akkaSlf4j      = "com.typesafe.akka" %% "akka-slf4j"      % Version.akka
-  val akkaTestkit    = "com.typesafe.akka" %% "akka-testkit"    % Version.akka
-  val logbackClassic = "ch.qos.logback"    %  "logback-classic" % Version.logback
-  val scalaTest      = "org.scalatest"     %% "scalatest"       % Version.scalaTest
+  val akkaActor       = "com.typesafe.akka" %% "akka-actor"                    % Version.akka
+  val akkaCluster     = "com.typesafe.akka" %% "akka-cluster"                  % Version.akka
+  val akkaContrib     = "com.typesafe.akka" %% "akka-contrib"                  % Version.akka
+  val akkaPersistence = "com.typesafe.akka" %% "akka-persistence-experimental" % Version.akka
+  val akkaSlf4j       = "com.typesafe.akka" %% "akka-slf4j"                    % Version.akka
+  val akkaTestkit     = "com.typesafe.akka" %% "akka-testkit"                  % Version.akka
+  val logbackClassic  = "ch.qos.logback"    %  "logback-classic"               % Version.logback
+  val scalaTest       = "org.scalatest"     %% "scalatest"                     % Version.scalaTest
+  val sprayCan        = "io.spray"          %% "spray-can"                     % Version.spray
+  val sprayJson       = "io.spray"          %% "spray-json"                    % Version.sprayJson
+  val sprayRouting    = "io.spray"          %% "spray-routing"                 % Version.spray
 }
 
 object Dependencies {
@@ -20,6 +28,16 @@ object Dependencies {
   import Library._
 
   val akkamazing = List(
+    akkaActor,
+    akkaCluster,
+    akkaContrib,
+    akkaPersistence,
+    akkaSlf4j,
+    logbackClassic,
+    sprayCan,
+    sprayJson,
+    sprayRouting,
+    akkaTestkit % "test",
     scalaTest % "test"
-  )
+  ) map (_.withSources())
 }
