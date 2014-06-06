0dea9500c5dc7befe3104f0b46323d458f5aede4
Heiko Seeberger <heiko.seeberger@gmail.com>
2014-06-06 13:08:17 +0200

    Set up project

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,18 +1,26 @@
 import sbt._
 
 object Version {
-  val akka      = "2.3.3"
+  val akka      = "2.3.4"
   val logback   = "1.1.2"
   val scala     = "2.11.1"
-  val scalaTest = "2.1.7"
+  val scalaTest = "2.2.0"
+  val spray     = "1.3.1"
+  val sprayJson = "1.2.6"
 }
 
 object Library {
-  val akkaActor      = "com.typesafe.akka" %% "akka-actor"      % Version.akka
-  val akkaSlf4j      = "com.typesafe.akka" %% "akka-slf4j"      % Version.akka
-  val akkaTestkit    = "com.typesafe.akka" %% "akka-testkit"    % Version.akka
-  val logbackClassic = "ch.qos.logback"    %  "logback-classic" % Version.logback
-  val scalaTest      = "org.scalatest"     %% "scalatest"       % Version.scalaTest
+  val akkaActor       = "com.typesafe.akka" %% "akka-actor"                    % Version.akka
+  val akkaCluster     = "com.typesafe.akka" %% "akka-cluster"                  % Version.akka
+  val akkaContrib     = "com.typesafe.akka" %% "akka-contrib"                  % Version.akka
+  val akkaPersistence = "com.typesafe.akka" %% "akka-persistence-experimental" % Version.akka
+  val akkaSlf4j       = "com.typesafe.akka" %% "akka-slf4j"                    % Version.akka
+  val akkaTestkit     = "com.typesafe.akka" %% "akka-testkit"                  % Version.akka
+  val logbackClassic  = "ch.qos.logback"    %  "logback-classic"               % Version.logback
+  val scalaTest       = "org.scalatest"     %% "scalatest"                     % Version.scalaTest
+  val sprayCan        = "io.spray"          %% "spray-can"                     % Version.spray
+  val sprayJson       = "io.spray"          %% "spray-json"                    % Version.sprayJson
+  val sprayRouting    = "io.spray"          %% "spray-routing"                 % Version.spray
 }
 
 object Dependencies {
@@ -20,6 +28,16 @@ object Dependencies {
   import Library._
 
   val akkamazing = List(
-    scalaTest % "test"
+    akkaActor,
+    akkaCluster,
+    akkaContrib,
+    akkaPersistence,
+    akkaSlf4j,
+    logbackClassic,
+    sprayCan,
+    sprayJson,
+    sprayRouting,
+    akkaTestkit % "test",
+    scalaTest   % "test"
   )
 }
